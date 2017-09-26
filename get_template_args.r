8d04d09b0ff8892e16583fb6dd0fa7768812eb38
Kirill Savine <kirill.savine@gmail.com>
2017-09-26 15:41:45 -0400

    Update get_template_args.r

--- get_template_args.r
+++ get_template_args.r
@@ -1,39 +1,53 @@
 
-get_template_args=function(x,directive=c("\\{\\{","\\}\\}")){
+function(x,directive=c("\\{\\{","\\}\\}")){
 
 
-	mypat=function(pat,x){as.numeric(gregexpr(pat,x)[[1]])}
-	
-	mypos=function(pat,x,positions){
-		res=lapply(x,function(y){mypat(pat,y)})
-		names(res)=positions
-		res
-	}
-	
-	get_item=function(x,st,en){
-		substr(x,st,en)
-	}
+        mypat=function(pat,x){as.numeric(gregexpr(pat,x)[[1]])}
+        
+        mypos=function(pat,x,positions){
+                res=lapply(x,function(y){mypat(pat,y)})
+                names(res)=positions
+                res
+        }
+        
+        get_item=function(x,st,en){
+                substr(x,st,en)
+        }
 
-	positions_st=grep(directive[1],x)
-	dat_st=x[positions_st]
+		melt_list=function(x){as.list(setNames(unlist(x, use.names=F),rep(names(x), lengths(x))))}
+		
+		trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
-	positions_en=grep(directive[1],x)
-	dat_en=x[positions_en]
-	
-	starts=mypos(directive[1],dat_st,positions_st)
-	ends=mypos(directive[2],dat_en,positions_en)
+		
+        positions_st=grep(directive[1],x)
+        dat_st=x[positions_st]
 
+        positions_en=grep(directive[1],x)
+        dat_en=x[positions_en]
+        
+        starts=melt_list(mypos(directive[1],dat_st,positions_st))
+        ends=melt_list(mypos(directive[2],dat_en,positions_en))
 
-	positions= data.frame(
-		pos = rep(names(positions_within), sapply(positions_within, length)),
-		st = unlist(starts),
-		en = unlist(ends)
-	)	
-	
-	positions=data.frame(lapply(positions,as.numeric),stringsAsFactors=FALSE)
-	positions$item=substr(dat_st[positions$pos],positions$st+2,positions$en-1)
-	
-	positions
-	#gsub(paste0(".*",directive[1],"\\s*|",directive[2],".*"), "", dat)
+        positions= data.frame(
+              #  pos = rep(names(positions_within), sapply(positions_within, length)),
+              #  pos = as.numeric(as.character(names(starts))),
+                pos = names(starts),
+                st = unlist(starts),
+                en = unlist(ends)
+        )       
+        
+		dat_st
+		dat_en
+		length(dat_st)
+		
+        positions2=data.frame(lapply(positions,as.numeric),stringsAsFactors=FALSE)
+    
+
+		positions2$item=trim(substr(dat_st[positions2$pos],positions2$st+2,positions2$en-1))
+        
+        positions2$pos=positions$pos
+		
+		positions2
+        #gsub(paste0(".*",directive[1],"\\s*|",directive[2],".*"), "", dat)
 }
 
